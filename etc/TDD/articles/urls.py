61df27cc4a5b5d06d0f8601aa68bed30b0d22f01
daeun <zerozero7bang@gmail.com>
2021-06-28 08:19:12 +0900

    add article CR

--- etc/TDD/articles/urls.py
+++ etc/TDD/articles/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index'),
-
     path('create/', views.create, name='create'),
     path('<int:pk>/', views.detail, name='detail'),
     path('<int:pk>/update/', views.update, name='update'),
--- etc/TDD/articles/views.py
+++ etc/TDD/articles/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
 
-def index(request):
-    pass
+from .models import Article, Comment
+from .serializers import ArticleListSerializer, ArticleSerializer, CommentListSerializer, CommentSerializer
+from django.contrib.auth import get_user_model
+
+# swagger
+from drf_yasg.utils import swagger_auto_schema
 
 
+@swagger_auto_schema(methods=['post'], request_body=ArticleSerializer)
+@api_view(['GET', 'POST'])
 def create(request):
-    pass
+    if request.method == 'GET':
+        articles = get_list_or_404(Article)
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
 
 def detail(request, pk):
     pass
