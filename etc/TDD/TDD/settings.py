0c64540413aec0d25c509ea050663c9ef707496e
daeun <zerozero7bang@gmail.com>
2021-06-28 05:55:13 +0900

    add models, urls, views skeleton

--- etc/TDD/TDD/settings.py
+++ etc/TDD/TDD/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'articles',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +126,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
--- etc/TDD/articles/views.py
+++ etc/TDD/articles/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    pass
+
+
+def create(request):
+    pass
+
+def detail(request, pk):
+    pass
+
+def delete(request, pk):
+    pass
+
+def update(request, pk):
+    pass
+
+
+def comments_create(request, pk):
+    pass
+
+def comments_delete(request, article_pk, comment_pk):
+    pass
