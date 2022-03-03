f50f6f6b70104146ee0832498c47b5d03eb3567c
이다은 <zerozero7bang@gmail.com>
2022-03-03 22:47:30 +0900

    ADD BOJ 13459 주석

--- BOJ/시뮬레이션/BOJ13459/BOJ13459.py
+++ BOJ/시뮬레이션/BOJ13459/BOJ13459.py
@@ -7,21 +7,26 @@ dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
 
+# 구슬 움직이기
 def move(color, pos, d, board):
     r, c = pos
     board[r][c] = "."
     while 1:
         next_position = board[r + dr[d]][c + dc[d]]
+        # 다음 위치로 이동할 수 있으면 이동
         if next_position == ".":
             r += dr[d]
             c += dc[d]
+        # 다음 위치가 탈출구면 탈출
         elif next_position == "O":
             return (-1, -1), board
+        # 벽이거나 다른 색 구슬 만나면 정지
         else:
             board[r][c] = color
             return (r, c), board
 
 
+# 먼저 움직이는 구슬 색깔 판별
 def first_second_move(d, red_pos, blue_pos):
     # 위 - 아래 - 왼쪽 - 오른쪽
     first, second = "R", "B"
@@ -47,19 +52,26 @@ def func(turn: int, board: list, d: int, red_pos, blue_pos) -> int:
     if red_pos == (-1, -1):
         return 1
 
+    # 먼저 움직이는 구슬 알아내기
     first, second = first_second_move(d, red_pos, blue_pos)
+    # red가 먼저 움직임
     if first == "R":
         red_pos, board = move("R", red_pos, d, board)
         blue_pos, board = move("B", blue_pos, d, board)
+    # blue가 먼저 움직임
     else:
         blue_pos, board = move("B", blue_pos, d, board)
         red_pos, board = move("R", red_pos, d, board)
 
+    # 상하좌우 방향으로 움직이기 재귀
     for i in range(4):
+        # 같은 방향 연속은 안봐도 됨
         if i == d:
             continue
+        # 새로 board 만들어서 넘겨서 재귀
         copy_board = [i[:] for i in board]
         ret = func(turn + 1, copy_board, i, red_pos, blue_pos)
+        # red 구슬이 탈출해서 끝나면 종료
         if ret:
             return 1
     return 0
@@ -75,6 +87,7 @@ for r in range(N):
         elif board[r][c] == "B":
             blue_pos = (r, c)
 
+# flag는 구슬 탈출했는지
 flag = 0
 for d in range(4):
     copy_board = [i[:] for i in board]
