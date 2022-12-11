0252b70d79ce7daadd8afbc7c0031e8563ae3a15
daeun <zerozero7bang@gmail.com>
2022-12-12 08:24:09 +0900

    boj1717

--- BOJ/BOJ1717/s1.py
+++ BOJ/BOJ1717/s1.py
@@ -2,17 +2,25 @@ import sys
 sys.stdin = open("input.txt")
 from pandas import DataFrame
 
+
+# 부모를 찾는 함수
+# 부모로 자기 자신을 가리키면 자기를 리턴하고
+# 부모로 다른 노드를 가리키면 그 노드를 찾아가서, 그 노드에서 부모를 다시 찾는다
 def find(x):
-    if p[x] != x:
-        p[x] = find(p[x])
-    return p[x]
+    if p[x] == x:
+        return x
+    else:
+        return find(p[x])
 
 
+# 합집합
+# y의 부모를 찾은 후, y 부모로 x 부모를 등록한다
 def union(x, y):
     p[find(y)] = find(x)
 
 
 n, m = map(int, input().split())
+# 초기에 모든 노드는 부모로 자기 자신을 가리킨다
 p = [i for i in range(n+1)]
 for _ in range(m):
     flag, a, b = map(int, input().split())
